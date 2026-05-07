beea89ee797eef58da579acb96ae7f4fb538565b
mfaizalbe <mdfaizalehsan@gmail.com>
2026-05-07 21:11:20 +0800

    terraform fmt

--- main.tf
+++ main.tf
@@ -26,27 +26,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "sctp-ce12-tfstate-bucket" # Change this
-    key    = "state.tfstate"            # Change this
-    region = "ap-southeast-1"
-  }
-}
-
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix = "terraform-faizal-" # Set your bucket name here
 }
