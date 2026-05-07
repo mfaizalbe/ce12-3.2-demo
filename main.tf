bb9efe90e08e28bded69649fcb1db9c8b9ff66ac
mfaizalbe <mdfaizalehsan@gmail.com>
2026-05-07 20:28:06 +0800

    fix terraform formatting

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
       version = "~> 5.0"
     }
   }
-  
+
   backend "s3" {
     bucket = "sctp-ce12-tfstate-bucket" # Change this
     key    = "state.tfstate"            # Change this
