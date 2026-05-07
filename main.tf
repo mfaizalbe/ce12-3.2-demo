8ec6e0a359081de4ae8b5ff10e999e609fee6ba9
mfaizalbe <mdfaizalehsan@gmail.com>
2026-05-07 21:16:12 +0800

    added to skip checkov

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-# checkov:skip=CKV_AWS_18 reason: not required for lab
-# checkov:skip=CKV_AWS_144 reason: not required for lab
-# checkov:skip=CKV_AWS_145 reason: not required for lab
-# checkov:skip=CKV2_AWS_62 reason: not required for lab
-# checkov:skip=CKV_AWS_300 reason: not required for lab
-# checkov:skip=CKV2_AWS_6 reason: not required for lab (if needed)
-
 provider "aws" {
   region = "ap-southeast-1"
 }
@@ -28,4 +21,12 @@ terraform {
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix = "terraform-faizal-" # Set your bucket name here
+  
+  # checkov suppressions
+  # checkov:skip=CKV_AWS_18
+  # checkov:skip=CKV_AWS_144
+  # checkov:skip=CKV_AWS_145
+  # checkov:skip=CKV2_AWS_62
+  # checkov:skip=CKV2_AWS_6
+  # checkov:skip=CKV2_AWS_61
 }
