8265f23fa16c4e5be6fc7bc36080f10685bdb145
mfaizalbe <mdfaizalehsan@gmail.com>
2026-05-07 20:08:42 +0800

    fix terraform formatting

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce12-tfstate-bucket" # Change this
-    key    = "state.tfstate"  # Change this
+    key    = "state.tfstate"            # Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix =  "terraform-faizal-" # Set your bucket name here
+  bucket_prefix = "terraform-faizal-" # Set your bucket name here
 }
